96b3e8f988c928eee75c64c1fff1890b6db123fe
Rrafa97 <1162965298@qq.com>
2021-04-20 12:12:11 +0800

    安装elementplus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(router).mount('#app')

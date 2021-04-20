dc71525bb61b1abc344061a4b919e6aa746f48a1
Rrafa97 <1162965298@qq.com>
2021-04-20 16:40:34 +0800

    完成轮播初始效果

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>
-    测试页面23
+   <div class="block">
+     11111
+    <el-carousel height="80vh">
+      <el-carousel-item v-for="item in imgurl" :key="item">
+         <el-image
+      style="width: 100%;"
+      :src="item"
+      :fit="fit"></el-image>
+      </el-carousel-item>
+    </el-carousel>
+    <div class="background"></div>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      imgurl: [
+        require("@/assets/images/home_ad1.png"),
+        require("@/assets/images/home_ad2.png"),
+        require("@/assets/images/home_ad3.png"),
+      ],
+    };
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.block {
+  height: auto;
+  background: #151618;
+  padding-top: 32px;
+}
+ .el-carousel__item h3 {
+    color: #151618;
+    background: #151618;
+    font-size: 14px;
+    line-height: 150px;
+    margin: 0;
+  }
+</style>

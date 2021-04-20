defc5a35ab03a0c64d9600777b3e545541bb77bb
Rrafa97 <1162965298@qq.com>
2021-04-20 14:35:58 +0800

    插入首页框架

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
---
+++ src/pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    测试页面23
+  </div>
+</template>

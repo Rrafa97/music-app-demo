3b4a93622fdd259e34b846aacfafdbeef3dd95ee
rrafa97 <1162965298@qq.com>
2021-04-23 17:54:36 +0800

    更改颜色

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="main-color">
     <div
       class="block"
       @touchstart="touchstart"
@@ -17,8 +17,13 @@
       </el-carousel>
       <div class="background"></div>
     </div>
-    <van-field v-model="state.text" label="输入搜索歌曲" />
-    <van-button type="danger" @click="serch">搜索</van-button>
+    <!-- <van-field class="main-color" v-model="state.text" label="输入搜索歌曲" /> -->
+    <van-field v-model="state.text" center clearable placeholder="输入搜索歌曲">
+      <template #button>
+        <van-button size="small" type="danger" @click="serch">搜索</van-button>
+      </template>
+    </van-field>
+    <!-- <van-button type="danger" @click="serch">搜索</van-button> -->
     <div v-for="(index, item) in songs" key="item">
       <!-- <van-card
         v-if="cardShow"
@@ -33,30 +38,33 @@
         :desc="'艺术家：' + index.artists[0].name"
         :title="'歌曲名:  ' + index.name"
         :thumb="index.artists[0].img1v1Url"
-        @click="getSong(index.id)"
+        @click="getSong(index.id,index)"
+        class="main-color"
       >
         <template #tags>
-          <van-tag plain type="danger">专辑：{{ index.album.name }}</van-tag>
-        </template>
-        <template #footer>
-          <van-button
-            :round="true"
-            color="linear-gradient(to right, #ff6034, #ee0a24)"
+          <van-tag class="main-color" plain type="danger"
+            >专辑：{{ index.album.name }}</van-tag
           >
-            在线播放
-          </van-button>
         </template>
       </van-card>
     </div>
-    <van-popup
+    <!-- <van-popup
       v-model:show="popShow"
       position="bottom"
-      :style="{ height: '30%' }"
+      :style="{ height: '10%' }"
     >
       <audio v-if="playShow" controls>
         <source :src="currentMp3" type="audio/mpeg" />
       </audio>
-    </van-popup>
+    </van-popup> -->
+    <van-sticky v-if="popShow" :offset-bottom="0" position="bottom">
+      <div class="mask-play">
+        <p>{{'‘' + playInfo.name + '’' + '-'  + playInfo.artists[0].name+ '——专辑:' + playInfo.album.name}}</p>
+        <audio v-if="playShow" controls>
+        <source :src="currentMp3" type="audio/mpeg" />
+      </audio>
+      </div>
+    </van-sticky>
   </div>
 </template>
 
@@ -80,12 +88,13 @@ export default {
       playShow: false,
       currentMp3: "",
       popShow: false,
+      playInfo: false
     };
   },
   setup() {
     const state = reactive({
       tel: "",
-      text: "lte",
+      text: "",
       digit: "",
       number: "",
       password: "",
@@ -121,17 +130,29 @@ export default {
         : false;
     },
     serch() {
-      SERCH_KEYWORDS(this.state.text).then((res) => {
-        this.songs = res.data.result.songs;
-        console.log(this.songs[0].album.name);
-        this.cardShow = true;
-      });
+      if (this.state.text === "") {
+        this.$toast.fail("输入关键词");
+      } else {
+        SERCH_KEYWORDS(this.state.text).then((res) => {
+          this.songs = res.data.result.songs;
+          console.log(this.songs[0].album.name);
+          this.cardShow = true;
+        });
+      }
     },
-    getSong(id) {
+    setPlayInfo(song) {
+      this.playInfo = song
+      console.log(this.playInfo)
+    },
+    getSong(id,song) {
       this.currentMp3 = "";
       this.playShow = false;
+      this.setPlayInfo(song)
       GET_SONG(id).then((res) => {
         console.log(res.data.data[0].url);
+        if(res.data.data[0].url === '') {
+          this.$toast.fail("暂时没有资源")
+        }
         this.currentMp3 = res.data.data[0].url;
         this.popShow = true;
         this.playShow = true;
@@ -148,7 +169,7 @@ export default {
 };
 </script>
 
-<style lang="less" scoped>
+<style lang="scss" scoped>
 .block {
   height: auto;
   background: #151618;
@@ -159,5 +180,17 @@ export default {
   font-size: 14px;
   line-height: 150px;
   margin: 0;
+}
+.main-color {
+  color: aliceblue;
+  background: rgba($color: #000000, $alpha: 0);
+}
+.mask-play {
+  padding: 16px 0;
+  background: rgba($color: #000000, $alpha: 0.5);
+
+  p {
+    margin-bottom: 16px;
+  }
 }
 </style>

0051bdead68df420c98f2012f5b5fc49ff585997
rrafa97 <1162965298@qq.com>
2021-04-27 09:41:40 +0800

    完善唱片UI，歌词页面播放

--- src/pages/lyrics.vue
+++ src/pages/lyrics.vue
@@ -26,9 +26,26 @@
         src="http://cdn.jsdelivr.net/gh/zjkwdy/website-cdn/assets/images/other/playing_stylus_lp_bg.png"
         alt=""
       /> -->
-      <audio autoplay ref="audio" controls>
+      <audio autoplay ref="audioPlay" controls>
         <source :src="currentMp3" type="audio/mpeg" />
       </audio>
+      <div class="lyric_area">
+        <ul class="lyric" ref="lyric">
+          {{
+            currentLyric
+          }}
+        </ul>
+        <ul ref="lyric">
+          {{
+            currentLyric
+          }}
+        </ul>
+        <ul ref="lyric">
+          {{
+            currentLyric
+          }}
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -37,6 +54,7 @@
 import { GET_LYRIC, GET_SONG, SONGS_DETAIL } from "../api/index.js";
 import { formlrc } from "@/utils"
 import { useRoute } from "vue-router";
+import { ref } from 'vue'
 export default {
   data () {
     return {
@@ -45,7 +63,6 @@ export default {
       PlayLyric: [],
       currentMp3: '',
       audioShow: false,
-      ad: "https://p2.music.126.net/6foZcR0P2Pdw0HRIdV1aiQ==/109951165914949830.jpg"
     }
   },
   watch: {
@@ -61,6 +78,9 @@ export default {
   setup () {
     const currentMp3 = useRoute().query.mp3
     const info = useRoute().query.playInfo
+    const audioPlay = ref(null)
+    const lyric = ref(null)
+    var currentLyric = ''
     var resShow = true
     let song = {}
     SONGS_DETAIL(useRoute().query.id).then(res => {
@@ -69,25 +89,38 @@ export default {
       console.log(song)
     })
     console.log(info)
-    return { info, currentMp3, song, resShow }
+    return { info, currentMp3, song, resShow, audioPlay, lyric, currentLyric }
   },
   mounted () {
-    // let te = "[00:00.000] 作词 : 吕易秋@SBMS Beijing↵[00:01.000] 作曲 : 郑楠@SBMS Beijing↵[00:02.000] 编曲 : 郑楠@SBMS Beijing↵[00:03.000] 制作人 : 郑楠@SBMS Beijing↵[00:18.803]数不尽 浩瀚的天体↵[00:22.868]银河是 梦想的轨迹↵[00:27.220]星光温暖 了↵[00:31.738]最渺小的 我↵[00:36.322]亿万个 人类的梦里↵[00:40.239]寄宿着 亿万个神明↵[00:44.809]痴心妄想 过↵[00:49.077]才会成真 呢↵[00:53.896]遥不可及 那朵流星 在深夜里↵[01:00.234]赐予天际 刹那的美丽↵[01:04.540]让这世界 都鲜活↵[01:10.783]我热爱 布满荆棘的生命↵[01:15.918]看平凡闪烁出奇迹↵[01:20.501]这刺破黑暗的火花↵[01:27.444]让我还奔跑着↵[01:29.245]想拥抱 炽热的繁星↵[01:33.360]哪怕注定燃烧殆尽↵[01:37.936]就这样固执不悔改↵[01:43.257]永不停歇的热爱（Whoa）↵[01:53.671]永不停歇的热爱（Whoa）"
-    console.log(this.$route.query.id)
-    // GET_SONG(this.$route.query.id).then(res => {
-    //   this.currentMp3 = res.data.data[0].url
-    //   this.audioShow = true
-    //   console.log(res.data.data[0].url)
-    // })
+    // console.log(this.makeDurationToSeconds(this.ts))
+    this.getTime()
     GET_LYRIC(this.$route.query.id).then(res => {
       this.lyric = formlrc(res.data.lrc.lyric)
-      // console.log(this.lyric)
+      console.log(this.lyric[0].lrc)
+      this.currentLyric = this.lyric[0].lrc
     })
   },
   methods: {
     sortRule (a, b) {
       return a.time - b.time;
-    }
+    },
+    getTime () {
+      setInterval(() => {
+        // console.log(this.audioPlay.currentTime)
+      }, 3000);
+      let that = this
+      this.audioPlay.ontimeupdate = function () {
+        // console.log(that.audioPlay.currentTime)
+      }
+    },
+    mainLy () {
+      var lineNo = 0
+      var C_pos = 6
+      var offset = -20
+      var audio = this.audioPlay
+      var ul = this.lyric
+
+    },
 
   }
 }
@@ -197,7 +230,7 @@ audio {
 .stylus_l {
   position: absolute;
   transform: scale(0.6);
-  top: -50%;
+  top: -38%;
   right: 0;
   z-index: 3000;
 }
@@ -205,11 +238,32 @@ audio {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   position: relative;
   margin-top: 64px;
 }
 .record {
   height: 100vw;
   width: 100vw;
+}
+.lyric_area {
+  height: 300px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 15px;
+}
+#lyric {
+  line-height: 20px;
+  transition-duration: 600ms;
+}
+#lyric .lineHigh {
+  color: red;
+}
+.lyric {
+  font-size: 24px;
+  margin: 16px;
 }
 </style>
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,9 +3,17 @@ export function formlrc (lrc) {
   let lrcarr = []
   arr.forEach((item, index) => {
     let lrco = {}
-    lrco.time = item.split(']')[0].replace("[", "")
+    lrco.time = makeDurationToSeconds(item.split(']')[0].replace("[", ""))
     lrco.lrc = item.split(']')[1]
     lrcarr[index] = lrco
   })
   return lrcarr
+}
+
+export function makeDurationToSeconds (time) {
+  var str = time;
+  var arr = str.split(/[:.]/);
+  var str_ = parseInt(arr[0])*60 + parseInt(arr[1])
+  // let arr_ = arr[1].split('.')
+  return str_
 }

64e9425cdd356d0755656709d54ea0651b916cfb
rrafa97 <1162965298@qq.com>
2021-04-29 19:11:38 +0800

    热点歌单搭建

--- src/pages/playlist_hot.vue
+++ src/pages/playlist_hot.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-    <van-tabs v-model:active="active" swipeable>
-      <van-tab v-for="index in 4" :title="'选项 ' + index">
-        内容 {{ index }}
+    <!-- <van-tabs v-model:active="active" swipeable>
+      <van-tab v-for="(index,item,i) in playListCategories" :title="index">
+        item
       </van-tab>
-    </van-tabs>
+    </van-tabs> -->
+    <div v-if="Object.keys(playListCategories).length > 1" v-for="(item,i) in playListCategories">
+      {{1111}}
+    </div>
   </div>
 </template>
 
-<script lang="js">
-import {PLAYLIST_CATLIST__} from './common_func.ts'
+<script lang="ts">
+import {PLAYLIST_CATLIST} from '@/api/index'
+import { onMounted } from 'vue';
 export default {
   setup() {
-    console.log(PLAYLIST_CATLIST__())
+    var playListCategories: Array<string> = []
+    PLAYLIST_CATLIST().then( res=> {
+      
+      playListCategories = res.data.categories
+      console.log(playListCategories)
+    })
+    onMounted( () => {
+      setTimeout(() => {
+         console.log(Object.keys(playListCategories).length)
+      }, 300);
+      console.log(playListCategories)
+    })
+    return {playListCategories}
   },
 };
 </script>

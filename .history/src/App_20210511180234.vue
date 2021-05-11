971dd2f28493c0fc7902d2cf792a6fd1f69c4f25
rrafa97 <1162965298@qq.com>
2021-05-11 18:03:34 +0800

    歌手搜索清单

---
+++ .history/src/App_20210511180234.vue
@@ -0,0 +1,41 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <keep-alive :include="cachedViews" :exclude="normalViews">
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+
+<script>
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
+export default {
+  name: 'App',
+  setup () {
+    let cachedViews = ['home']
+    let normalViews = ['lyrics']
+    function router (to, from) {
+      if (!cachedViews.includes(to.name) && !normalViews.includes(to.name)) {
+        cachedViews.push(to.name)
+      }
+    }
+    return { cachedViews, normalViews }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* background: rgb(98, 22, 36); */
+  /* margin-top: 60px; */
+}
+</style>
---
+++ .history/src/components/singerlist_20210511180307.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    11
+  </div>
+</template>
+
+<style lang="less" scoped>
+div {
+  color: black;
+}
+</style>

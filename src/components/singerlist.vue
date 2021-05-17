055f5a8eded69dc8cb567b743511f0b33b263eaf
rrafa97 <1162965298@qq.com>
2021-05-17 10:31:24 +0800

    更改主题色调

--- src/components/singerlist.vue
+++ src/components/singerlist.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
+    <van-grid  style="background:white" :column-num="2">
+      <van-grid-item v-for="item in compData.artists">
+        <!-- <template #icon> -->
+          <van-image :style="{ height: '64px',width: '64px' }" :src=item.img1v1Url></van-image>
+        <!-- </template> -->
+        <!-- {{item.img1v1Url}} -->
+        <p :style="{fontSize:'12px',color: 'white'}">{{item.name}}</p>
+        </van-grid-item>
+</van-grid>
     11{{compData}}
+    
   </div>
 </template>
 

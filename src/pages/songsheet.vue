8c377b6b76a16bf67d878294be75f85612213340
rrafa97 <1162965298@qq.com>
2021-05-18 17:40:24 +0800

    更新搜索歌单

--- src/pages/songsheet.vue
+++ src/pages/songsheet.vue
@@ -1,10 +1,37 @@
 <template>
-  <div v-for="item in compData.playlists">
-    <van-image width="6rem" height="6rem" fit="cover" :src="item.coverImgUrl" />
+  <div>
+    <!-- <span :style="{ textAlign: 'center' }" v-for="item in compData.playlists">
+      <van-image
+        :style="{ width: '40vw', height: '40vw', margin: '16px' }"
+        width="6rem"
+        height="6rem"
+        fit="cover"
+        radius="16px"
+        :src="item.coverImgUrl"
+        >123123</van-image
+      >
+    </span> -->
+    <van-grid :column-num="3" icon-size="96" :center='false'>
+      <van-grid-item
+        v-for="item in compData.playlists"
+        :icon="item.coverImgUrl"
+        :text="item.name + '—播放：' + item.playCount"
+        @click="toSongsList(item.id)"
+      >
+      <template #icon>
+        <van-image radius='12px' :src="item.coverImgUrl"/>
+      </template>
+      <template #text>
+        <div :style="{fontSize: '14px',color: 'rgba(0, 0, 0,.7)'}">{{item.name}}</div>
+        <p :style="{fontSize: '12px',color: 'rgba(18, 110, 130,.6)'}">播放：{{item.playCount}}</p>
+      </template>
+      </van-grid-item>
+    </van-grid>
   </div>
 </template>
 
 <script lang="ts">
+import { PLAYLIST_INFO} from "@/api/index";
 export default {
   props: ['compData'],
   setup(props: any) {
@@ -15,5 +42,15 @@ export default {
 
     return { state }
   },
+  methods: {
+    toSongsList(id:any) {
+      PLAYLIST_INFO(id).then( res => {
+        console.log(res)
+      })
+    }
+  }
 }
 </script>
+
+<style lang="less" scoped>
+</style>
